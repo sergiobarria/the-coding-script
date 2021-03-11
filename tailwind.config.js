0a3122fa23da1320ef08bc516c0a15a4c7b104b5
Sergio Barria <sergio.barria@icloud.com>
2021-03-11 00:12:09 -0500

    changed purge components directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const colors = require("tailwindcss/colors")
 module.exports = {
   purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
