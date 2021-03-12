4606a74405832e8f2afe3953f73ddd04c147e6d4
Sergio Barria <sergio.barria@icloud.com>
2021-03-11 21:45:18 -0500

    changed project id

--- lib/sanity/config.ts
+++ lib/sanity/config.ts
@@ -1,7 +1,9 @@
 import sanityClient from "@sanity/client"
 
 export default sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_ID,
+  projectId: "ss0ub4d0",
   dataset: "production",
-  useCdn: false, // false if you want to ensure fresh data
+  useCdn: true, // false if you want to ensure fresh data
 })
+
+// process.env.NEXT_PUBLIC_SANITY_ID
