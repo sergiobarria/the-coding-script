a8f3b86cce48137e3dd5166c7193adc7b054a6ac
Sergio Barria <sergio.barria@icloud.com>
2021-03-12 14:53:26 -0500

    added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ import Document, {
   DocumentContext,
 } from "next/document"
 
+import { GA_TRACKING_ID } from "../../lib/analytics/gtag"
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -18,6 +20,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-199V1DC52P"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_TRACKING_ID}');
+            `,
+            }}
+          ></script>
           <link
             href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
             rel="stylesheet"
