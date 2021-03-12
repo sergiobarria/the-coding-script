49e4c1b8b4e98aaf90c398933ab013bac058666b
Sergio Barria <sergio.barria@icloud.com>
2021-03-11 21:40:34 -0500

    changed useCdn to false

--- lib/sanity/config.ts
+++ lib/sanity/config.ts
@@ -3,5 +3,5 @@ import sanityClient from "@sanity/client"
 export default sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_ID,
   dataset: "production",
-  useCdn: true,
+  useCdn: false, // false if you want to ensure fresh data
 })
