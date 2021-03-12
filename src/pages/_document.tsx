daf51eb3ac3128ede7e1dddf5fe853ff39cfd049
Sergio Barria <sergio.barria@icloud.com>
2021-03-12 15:22:19 -0500

    changed position of GA scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
           <script
             async
@@ -36,14 +45,6 @@ class MyDocument extends Document {
             `,
             }}
           ></script>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
